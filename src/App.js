a8df071752fbff078945ef30fbec88231bc55b75
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-08-26 15:31:25 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { AnimatedSwitch } from 'react-router-transition';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 
@@ -10,6 +11,10 @@ import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
+import Trip from './components/views/Trip/TripContainer';
+import Country from './components/views/Country/CountryContainer';
 
 import parseTrips from './utils/parseTrips';
 import {setMultipleStates} from './redux/globalRedux';
@@ -37,13 +42,22 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch location={location}>
+          <AnimatedSwitch location={location}
+            atEnter={{ opacity: 0 }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1 }}
+            className={StyleSheet.switchWrapper}
+          >
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
             {/* TODO - add more routes for other views */}
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
-          </Switch>
+            <Route exact path='/Countries' component={Countries} />
+            <Route exact path='/Regions' component={Regions} />
+            <Route exact path='/trip/:id' component={Trip} />
+            <Route exact path='/country/:id' component={Country} />
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
